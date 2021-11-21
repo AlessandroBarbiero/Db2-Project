438e0f1f8369fa640255e21e0ee3b1665d343608
AlessandroBarbiero <alessandro.barbiero@mail.polimi.it>
2021-11-21 20:29:42 +0100

    created UserEntity and set the relation ManyToOne in OrderEntity

--- src/main/java/it/polimi/Db2_Project/entities/OrderEntity.java
+++ src/main/java/it/polimi/Db2_Project/entities/OrderEntity.java
@@ -1,5 +1,41 @@
 package it.polimi.Db2_Project.entities;
 
-public class OrderEntity
-{
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order", schema = "db2_database")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable=false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn ( name = "userId")
+    private UserEntity user;
+
+    public OrderEntity() {
+    }
+
+//%%%%%%%%%%% GETTERS %%%%%%%%%%%%%%
+
+    public int getId() {
+        return id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+//%%%%%%%%%%% SETTERS %%%%%%%%%%%%%%
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+        //todo: probably there is something to add for database updating
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/it/polimi/Db2_Project/entities/UserEntity.java
+++ src/main/java/it/polimi/Db2_Project/entities/UserEntity.java
@@ -1,6 +1,84 @@
 package it.polimi.Db2_Project.entities;
 
-public class UserEntity
-{
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "user", schema = "db2_database")
+public class UserEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable=false)
+    private int id;
+
+    @Column(name = "username", unique=true, nullable=false)
+    private String username;
+
+    @Column(name = "password", nullable=false)
+    private String password;
+
+    @Column(name = "email", unique=true, nullable=false)
+    private String email;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy="userId", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderEntity> orders;
+
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+//%%%%%%%%%%%%%%%%%% GETTERS %%%%%%%%%%%%%%%%%%%%%%%%
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<OrderEntity> getOrders() {
+        return orders;
+    }
+
+//%%%%%%%%%%%%%%%%%% SETTERS %%%%%%%%%%%%%%%%%%%
+
+    public void setId(int user_id) {
+        this.id = user_id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setOrders(List<OrderEntity> orders) {
+        this.orders = orders;
+    }
 }
